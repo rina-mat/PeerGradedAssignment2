bb728f98d364d8cfa48a6cf3e0a48093a1317c62
Rina Paul <rinapaulsust@gmail.com>
2020-06-29 23:38:23 +0600

    This is the first commit.

---
+++ cacheMatrix.R
@@ -0,0 +1,35 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    print(x)
+    inv <<- NULL 
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- solve(x)    
+  getinv <- function() inv 
+  list(set = set, get = get,
+       setinv = setinv ,
+       getinv = getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinv()  
+  if(!is.null(inv)) {  
+    message("getting cached data")
+    return(inv) 
+  }
+  data <- x$get()
+  inv <- solve(data, ...)  
+  x$setinv(inv) 
+  inv
+}
